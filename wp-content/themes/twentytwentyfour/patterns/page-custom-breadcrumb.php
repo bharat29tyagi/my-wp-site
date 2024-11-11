d500650d2d8356d48a0f836c04d47dbe849064e4
bharat29tyagi <bharat29tyagi@gmail.com>
2024-11-11 17:39:47 +0530

    first commit

---
+++ wp-content/themes/twentytwentyfour/patterns/page-custom-breadcrumb.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Title: Page Location Indicator
+ * Slug: twentytwentyfour/page-custom-breadcrumb
+ * Categories: twentytwentyfour_page
+ * Inserter: no
+ */
+ 
+if (function_exists('custom_breadcrumbs')) custom_breadcrumbs(); ?>
